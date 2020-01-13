1e328456626e9346bc040b7d28adac616363f6da
Michael Underwood <mike.underwood@gmail.com>
2020-01-13 08:28:10 -0700

    WIP second approach, Vue SFC to be compiled
    This approach was working well with `vue build example.vue` (or the task
    `npm run example`), until an update to Vue CLI deprecated the build
    command. Now need to work on the correct way to build and serve the
    component in a dev environment.

---
+++ example.vue
@@ -0,0 +1,85 @@
+<template>
+  <l-map
+    style="height: 400px"
+    :center="mapCentre"
+    :zoom="mapZoom"
+    @polylinemeasure:toggle="handleToggle"
+    @polylinemeasure:start="handleStart"
+    @polylinemeasure:resume="handleResume"
+    @polylinemeasure:finish="handleFinish"
+    @polylinemeasure:clear="handleClear"
+    @polylinemeasure:add="handleAdd"
+    @polylinemeasure:insert="handleInsert"
+    @polylinemeasure:move="handleMove"
+    @polylinemeasure:remove="handleRemove"
+  >
+    <l-tile-layer
+      :url="tileUrl"
+      :attribution="tileAttribution"
+    />
+    <l-control-polyline-measure />
+  </l-map>
+</template>
+
+<script>
+import L from 'leaflet';
+import { LMap, LTileLayer } from 'vue2-leaflet';
+import LControlPolylineMeasure from './src/LControlPolylineMeasure.vue';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LControlPolylineMeasure,
+  },
+  data () {
+    return {
+      mapCentre: [51, -114],
+      mapZoom: 10,
+      tileUrl: '//{s}.tile.osm.org/{z}/{x}/{y}.png',
+      tileAttribution: '&copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors',
+    };
+  },
+  methods: {
+    handleToggle (e) {
+      console.log('Toggled', e.status);
+    },
+
+    handleStart (currentLine) {
+
+    },
+
+    handleResume (currentLine) {
+
+    },
+
+    handleFinish (e) {
+
+    },
+
+    handleClear (e) {
+
+    },
+
+    handleAdd (e) {
+
+    },
+
+    handleInsert (e) {
+
+    },
+
+    handleMove (e) {
+
+    },
+
+    handleRemove (e) {
+
+    },
+  },
+};
+</script>
+
+<style>
+@import '~leaflet/dist/leaflet.css';
+</style>
